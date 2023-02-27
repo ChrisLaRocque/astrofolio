a4ee363ffb75370011bd966da68496932585bb61
Chris LaRocque <larocque.christopher@gmail.com>
2023-02-27 08:06:03 -0500

    Use actual origin instead of hardcoding

--- utils/ogImageURL.js
+++ utils/ogImageURL.js
@@ -1,5 +1,5 @@
-export default function ogImageURL({ title, subtitle }) {
-	return `https://larocque.dev/.netlify/functions/og-image?title=${encodeURIComponent(
+export default function ogImageURL({ title, subtitle, origin }) {
+	return `${origin}/.netlify/functions/og-image?title=${encodeURIComponent(
 		title
 	)}&subtitle=${encodeURIComponent(subtitle)}`;
 }
