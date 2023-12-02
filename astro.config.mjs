6212c5e4b63bc00fc5ad6e8ddaa6ee34c80c912d
ChrisLaRocque <larocque.christopher@gmail.com>
2023-12-01 20:49:09 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import partytown from "@astrojs/partytown";
 import serviceWorker from "astrojs-service-worker";
 import { remarkReadingTime } from "./utils/remark-reading-time.mjs";
