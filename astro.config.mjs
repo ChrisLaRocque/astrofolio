08a7706cae2b4e01f9be59e9b9067a62394a02a8
ChrisLaRocque <larocque.christopher@gmail.com>
2025-03-11 15:10:40 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [
     tailwind(),
     partytown({
-      config: { forward: ["dataLayer.push"] },
+      config: { forward: [["dataLayer.push", { preserveBehavior: true }]] },
     }),
     mdx(),
   ],
