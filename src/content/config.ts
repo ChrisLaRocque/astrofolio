0483197415ed3843b9b6305361155803cec27cc6
ChrisLaRocque <larocque.christopher@gmail.com>
2025-09-27 21:46:41 -0400

    Fix type errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,31 +1,31 @@
 // 1. Import your utilities and schemas
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection } from 'astro:content';
 
-const techItem = z.enum([
-  "graphql",
-  "html",
-  "tailwind-css",
-  "github",
-  "gitlab",
-  "npm",
-  "css",
-  "javascript",
-  "nodejs",
-  "aws",
-  "netlify",
-  "contentful",
-  "nuxt",
-  "vue",
-  "react",
-  "gatsby",
-  "sass",
-  "bulma",
-  "next-js",
-  "astro",
-  "typescript",
-  "yarn",
-  "vitest",
-  "sanity"
+export const techItem = z.enum([
+  'graphql',
+  'html',
+  'tailwind-css',
+  'github',
+  'gitlab',
+  'npm',
+  'css',
+  'javascript',
+  'nodejs',
+  'aws',
+  'netlify',
+  'contentful',
+  'nuxt',
+  'vue',
+  'react',
+  'gatsby',
+  'sass',
+  'bulma',
+  'next-js',
+  'astro',
+  'typescript',
+  'yarn',
+  'vitest',
+  'sanity',
 ]);
 
 // 2. Define your collections
